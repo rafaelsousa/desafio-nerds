b2a30057bfad7ee18008bcf166c3efd87759a636
Roberson Faria <roberson.faria@gmail.com>
2021-05-21 00:23:07 -0300

    create vuejs front

---
+++ php-vuejs/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <list-person></list-person>
+  </div>
+</template>
+
+<script>
+import ListPerson from './components/ListPerson'
+
+export default {
+  name: 'App',
+  components: {
+    ListPerson
+  }
+}
+</script>
+
+<style>
+</style>
